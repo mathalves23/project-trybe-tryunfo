bcfc8b2449884bd22d86622f0454fee685755b8e
Matheus Alves <matheus.aalves@hotmail.com>
2022-01-11 10:10:20 -0300

    Requisito 6

--- src/App.js
+++ src/App.js
@@ -17,26 +17,60 @@ class App extends React.Component {
       cardAttr2: '0',
       cardAttr3: '0',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
       arrayCards: [],
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.saveCard = this.saveCard.bind(this);
   }
-
-  // Sem desestruturar:
-  // handleChange = (event) => {
-  //   this.setState({
-  //     [event.target.name]: event.target.value,
-  //   }, this.formValidation);
-  // }
 
   // Desestruturado:
   handleChange = ({ target: { name, value } }) => {
     this.setState({
       [name]: value,
-    }, this.formValidation);
+    },
+    this.formValidation,
+    this.saveCard);
+  }
+
+  saveCard() {
+    const {
+      arrayCards,
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+    } = this.state;
+    const card = {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    };
+    this.setState({
+      arrayCards: [...arrayCards, card],
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      hasTrunfo: cardTrunfo || hasTrunfo,
+    });
   }
 
   formValidation() {
@@ -61,43 +95,6 @@ class App extends React.Component {
     this.setState({ isSaveButtonDisabled: !okForm });
   }
 
-  saveCard() {
-    const {
-      arrayCards,
-      cardName,
-      cardDescription,
-      cardAttr1,
-      cardAttr2,
-      cardAttr3,
-      cardImage,
-      cardRare,
-      cardTrunfo,
-    } = this.state;
-    const card = {
-      cardName,
-      cardDescription,
-      cardAttr1,
-      cardAttr2,
-      cardAttr3,
-      cardImage,
-      cardRare,
-      cardTrunfo,
-    };
-    this.setState({
-      arrayCards: [...arrayCards, card],
-      cardName: '',
-      cardDescription: '',
-      cardAttr1: '0',
-      cardAttr2: '0',
-      cardAttr3: '0',
-      cardImage: '',
-      cardRare: '',
-      cardTrunfo: false,
-      hasTrunfo: false,
-      isSaveButtonDisabled: true,
-    });
-  }
-
   render() {
     const {
       cardName,
@@ -110,35 +107,45 @@ class App extends React.Component {
       cardTrunfo,
       hasTrunfo,
       isSaveButtonDisabled,
+      // arrayCards,
     } = this.state;
 
     return (
       <div>
-        <h1> Adicionar nova carta </h1>
-        <Form
-          cardName={ cardName }
-          cardDescription={ cardDescription }
-          cardImage={ cardImage }
-          cardRare={ cardRare }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
-          cardTrunfo={ cardTrunfo }
-          hasTrunfo={ hasTrunfo }
-          isSaveButtonDisabled={ isSaveButtonDisabled }
-          onInputChange={ this.handleChange }
-        />
-        <h2>Carta: </h2>
-        <Card
-          cardName={ cardName }
-          cardDescription={ cardDescription }
-          cardImage={ cardImage }
-          cardRare={ cardRare }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
-          cardTrunfo={ cardTrunfo }
-        />
+        <h1> SUPER TRYUNFO </h1>
+        <section id="form">
+          <h2> Adicionar nova carta </h2>
+          <Form
+            cardName={ cardName }
+            cardDescription={ cardDescription }
+            cardImage={ cardImage }
+            cardRare={ cardRare }
+            cardAttr1={ cardAttr1 }
+            cardAttr2={ cardAttr2 }
+            cardAttr3={ cardAttr3 }
+            cardTrunfo={ cardTrunfo }
+            hasTrunfo={ hasTrunfo }
+            isSaveButtonDisabled={ isSaveButtonDisabled }
+            onInputChange={ this.handleChange }
+            onSaveButtonClick={ this.saveCard }
+          />
+        </section>
+        <section id="card">
+          <h2>Carta: </h2>
+          <Card
+            cardName={ cardName }
+            cardDescription={ cardDescription }
+            cardImage={ cardImage }
+            cardRare={ cardRare }
+            cardAttr1={ cardAttr1 }
+            cardAttr2={ cardAttr2 }
+            cardAttr3={ cardAttr3 }
+            cardTrunfo={ cardTrunfo }
+          />
+        </section>
+        <section id="list">
+          <h2>Lista de cartas: </h2>
+        </section>
       </div>
     );
   }
